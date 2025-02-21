b0c5d338df111fc3fd882b1bfea6f99ab740901b
NegarAsg <110455655+NegarAsg@users.noreply.github.com>
2025-02-21 14:52:28 +0100

    Search functionality using the API is added

--- src/app.js
+++ src/app.js
@@ -1,9 +1,26 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let updatedTemperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(updatedTemperature);
+}
+
+function searchCity(city) {
+  let apiKey = "t66df09o88f95a5ba815f1b8024034ae";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric
+`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Tehran");
